2bb5f7330d96dabccd47f0c10b25dd6a2c073050
Travis Fischer <fisch0920@gmail.com>
2018-06-23 19:50:16 -0400

    better CLI usage

---
+++ index.test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const { test } = require('ava')
+const execa = require('execa')
+
+test('--help', async (t) => {
+  const { stdout } = await execa('./index.js', [ '--help' ])
+  t.true(stdout.length > 0)
+  t.true(/create-react-library/.test(stdout))
+})
