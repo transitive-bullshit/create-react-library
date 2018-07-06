0aa320c1fa326bad9bf7b7330d028c82f18c9e5e
Travis Fischer <fisch0920@gmail.com>
2018-07-06 15:49:56 -0400

    add sourcemap support to rollup template

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -12,11 +12,13 @@ export default {
   output: [
     {
       file: pkg.main,
-      format: 'cjs'
+      format: 'cjs',
+      sourcemap: true
     },
     {
       file: pkg.module,
-      format: 'es'
+      format: 'es',
+      sourcemap: true
     }
   ],
   plugins: [
