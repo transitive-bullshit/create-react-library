a4b8087098602a97ff4803c7ac6a32697809ee91
vitormalencar <vitor.maia.alencar@gmail.com>
2018-07-15 15:06:55 +0200

    fixing typo

--- template/default/src/index.stories.js
+++ template/default/src/index.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 
 import ExampleComponent from './index';
 
-let stories = storiesOf('ExampleComponents', module)
+let stories = storiesOf('ExampleComponent', module)
 
 stories.add('Default', () => <ExampleComponent text='From Storybook'/>)
