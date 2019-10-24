6a1340901f660077f4844139dd550c3c7b22ad1f
baldm0mma <jev.forsberg@gmail.com>
2019-10-24 13:21:53 -0600

    Change ExampleComponent import to named import in template test

--- template/default/src/test.js
+++ template/default/src/test.js
@@ -1,7 +1,7 @@
-import ExampleComponent from './'
+import { ExampleComponent } from './';
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+    expect(ExampleComponent).toBeTruthy();
+  });
+});
