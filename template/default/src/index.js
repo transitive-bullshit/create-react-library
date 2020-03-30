b6b4380e3941f3adf65fad3b24fd5da3a281f419
Travis Fischer <fisch0920@gmail.com>
2020-03-30 18:53:41 -0400

    fix: typescript template

--- template/default/src/index.js
+++ template/default/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './styles.css'
+import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
--- template/typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './styles.css'
-
-export const ExampleComponent = ({ text: string }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
