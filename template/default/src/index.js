95efb21fe5fe410caf2b82e05a7e16505dbdb41b
VTEX <vtex@vtexs-mbp.home>
2020-07-13 18:40:15 -0300

    removes alternative templates, sets vtex default

--- template/default/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './styles.module.css'
-
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
--- template/flow/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import React from 'react'
-import type { Element } from 'react'
-import styles from './styles.module.css'
-
-export const ExampleComponent = ({ text }): Element => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
