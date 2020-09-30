ccce4f1f7ffdb74fccbaa45cddf64423f512049a
frani1 <franinnocenti@gmail.com>
2020-09-30 09:51:58 -0300

    style code on all existed files with prettier

--- template/typescript/src/typings.d.ts
+++ template/typescript/src/typings.d.ts
@@ -3,23 +3,24 @@
  * will be overridden with file-specific definitions by rollup
  */
 declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
+  const content: { [className: string]: string }
+  export default content
 }
 declare module '*.scss' {
-  const content: { [className: string]: string };
-  export default content;
+  const content: { [className: string]: string }
+  export default content
 }
 declare module '*.less' {
-  const content: { [className: string]: string };
-  export default content;
+  const content: { [className: string]: string }
+  export default content
 }
 
-interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+interface SvgrComponent
+  extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 
 declare module '*.svg' {
-  const svgUrl: string;
-  const svgComponent: SvgrComponent;
-  export default svgUrl;
+  const svgUrl: string
+  const svgComponent: SvgrComponent
+  export default svgUrl
   export { svgComponent as ReactComponent }
 }
